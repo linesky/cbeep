101b06f1ef44d10811cdae1b12afe1305bcb0bef
sky line <32512284+linesky@users.noreply.github.com>
2022-04-19 00:02:50 +0100

    gcc beep.c -lwiringPi

--- beep.c
+++ beep.c
@@ -8,6 +8,7 @@ int main(int argc,char *argv[]){
 	int i;
 	int ii;
 	int n;
+	int nn;
 	double d;
 	double dd;
 	time_t t;
@@ -16,7 +17,7 @@ int main(int argc,char *argv[]){
 	i=wiringPiSetup();
 	if(i==-1)exit(1);
 	d=atof(argv[1]);
-	d=1/d;
+	d=1/d*500;
 	i=(int)d;
 	dd=atof(argv[2]);
 	ii=(int)dd;
@@ -25,9 +26,9 @@ int main(int argc,char *argv[]){
 	t+ii;
 	while(1){
 		digitalWrite(7,1);
-		usleep(i*1000000);
+		for(n=0;n<i;n++)usleep(1000);
 		digitalWrite(7,0);
-		usleep(i*1000000);
+		for(n=0;n<i;n++)usleep(1000);
 		if(time(NULL)>t)goto exits;
 	}
 	exits:
